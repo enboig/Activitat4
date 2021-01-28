3ec1a31a4272b355dd5f7610762bc60ac006d18c
enBoig <enboig@gmail.com>
2021-01-28 15:46:24 +0100

    Primera versió del dau

--- src/daus/Dau.java
+++ src/daus/Dau.java
@@ -1,6 +1,24 @@
 package daus;
 
+import java.util.Random;
+
 public class Dau {
+
+	private int cares = 0;
+	private Random rand;
+
+	public Dau() {
+		this.cares = 6;
+		rand = new Random();
+	}
+
+	public Dau(int cares) {
+		this.cares = cares;
+	}
+
+	public int tirada() {
+		return rand.nextInt(cares) + 1;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
